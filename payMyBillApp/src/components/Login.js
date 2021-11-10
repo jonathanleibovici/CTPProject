f911189cc496867a4d28d95cbc98f56db733b17e
jonathanleibovici <55033245+jonathanleibovici@users.noreply.github.com>
2021-11-10 11:27:12 -0500

    needstobefixed

--- payMyBillApp/src/components/Login.js
+++ payMyBillApp/src/components/Login.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import "./Login.css";
-import image from '../pictures/image.png';
+import image from "../pictures/image.png";
 import { Link } from "react-router-dom";
 // import { Switch, Route } from "react-router-dom";
-
 
 function Login() {
 	const {
@@ -18,35 +17,38 @@ function Login() {
 		console.log(data);
 	};
 	return (
-		<div className="Login-container">
-			<div className="header">PayMyBill</div>
-			<div className="content">
-				<form className="Form" onSubmit={handleSubmit(onSubmit)}>
-                <img src= {image} alt="Avatar" className="avatar"/>
-					<input
-						type="text"
-						placeholder="Email"
-						name="email"
-						{...register("email", {
-							required: "Required",
-						})}
-					/>
-					<div></div>
+		<div className="whole-container">
+			<div className="left-half-img-srs"></div>
+			<div className="Login-container-right-half">
+				<div className="header">PayMyBill</div>
+				<div className="content">
+					<form className="Form" onSubmit={handleSubmit(onSubmit)}>
+						<img src={image} alt="Avatar" className="avatar" />
+						<input
+							type="text"
+							placeholder="Email"
+							name="email"
+							{...register("email", {
+								required: "Required",
+							})}
+						/>
 
-					<input
-						type="password"
-						placeholder="password"
-						name="password"
-						{...register("password", {
-							required: "Required",
-							minLength: 8,
-						})}
-					/>
-					<div></div>
+						<input
+							type="password"
+							placeholder="password"
+							name="password"
+							{...register("password", {
+								required: "Required",
+								minLength: 8,
+							})}
+						/>
 
-					{errors.password && <p>password is invalid</p>}
-					<Link to = '/home'><input className="btn" type="submit"/></Link>
-				</form>
+						{errors.password && <p>password is invalid</p>}
+						<Link to="/home">
+							<input className="btn" type="submit" />
+						</Link>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
